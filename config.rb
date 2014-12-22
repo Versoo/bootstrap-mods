36c4e631fdf3cfc344501db7eb132e847b15e14a
Joshua Hunt <joshua.hunt@quantum.com>
2014-12-22 12:57:09 -0800

    Init

---
+++ config.rb
@@ -0,0 +1,14 @@
+# default Compass config
+# 	used for tools that don't/won't allow renaming the default config.rb
+
+# Require any additional compass plugins here.
+add_import_path 'bower_components/bootstrap-sass-official/assets/stylesheets/'
+
+# Set this to the root of your project when deployed:
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+
+# options
+output_style = :compressed
+line_comments = false
